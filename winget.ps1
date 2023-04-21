02edfcc963d642291244533012c645ee664d6b1e
Erik Elkins <erik.elkins@outlook.com>
2023-04-21 10:26:55 -0700

    Adding intial setup script

---
+++ winget.ps1
@@ -0,0 +1,7 @@
+winget install AgileBits.1Password
+winget install Python.Python.3.12
+winget install Google.Chrome
+winget install Valve.Steam
+winget install Microsoft.VisualStudioCode
+winget install Git.Git
+wsl --install
