2617431e988d59144887bc48a88a24a9780e3b5c
Erik Elkins <erik.elkins@outlook.com>
2023-04-21 10:54:59 -0700

    Adding two more

--- winget.ps1
+++ winget.ps1
@@ -4,4 +4,6 @@ winget install Google.Chrome
 winget install Valve.Steam
 winget install Microsoft.VisualStudioCode
 winget install Git.Git
+winget install RiotGames.Valorant.NA
+winget install Notion.Notion
 wsl --install
